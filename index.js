65c657ab084321eb33e37a25dcf4a00be155d83d
venom-712 <themonty712@gmail.com>
2022-07-25 14:10:00 +0530

    server completed

---
+++ index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  return res.send("App is running");
+});
+
+const CONNECTION_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
